8a518a16716ae13a7f6d0e726ab714861f231e1f
Łukasz Śliwa <lukasz.sliwa@apptamers.com>
2015-05-27 19:32:18 +0200

    Made url_with_version instance method

--- lib/api-client/configuration.rb
+++ lib/api-client/configuration.rb
@@ -1,7 +1,7 @@
 class Api::Client::Configuration
   attr_accessor :url, :version
 
-  def self.url_with_version
+  def url_with_version
     @url_with_version ||= [url, version].join '/'
   end
 end
