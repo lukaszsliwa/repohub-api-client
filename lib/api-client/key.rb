16fd305cc8d52528cd9ac9948dba505b0adb2f9d
Łukasz Śliwa <lukasz.sliwa@apptamers.com>
2015-05-28 16:53:39 +0200

    Fixed key endpoint path.

--- lib/api-client/key.rb
+++ lib/api-client/key.rb
@@ -1,3 +1,3 @@
 class Api::Client::Key < ActiveResource::Base
-  self.site = "#{Api::Client.configuration.url_with_version}/users/:user_id"
+  self.site = "#{Api::Client.configuration.url_with_version}/developers/:developer_id"
 end
