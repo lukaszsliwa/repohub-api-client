3df290cc84f6174415160d5f816edec0ce74bba7
Łukasz Śliwa <lukasz.sliwa@apptamers.com>
2015-05-29 19:28:17 +0200

    Put headers into Api::Client::Base

--- lib/api-client/base.rb
+++ lib/api-client/base.rb
@@ -1,3 +1,5 @@
 class Api::Client::Base < ActiveResource::Base
-  headers['api_key'] = Api::Client.configuration.api_key
+  def self.headers
+    @headers ||= {'api_key': Api::Client.configuration.api_key}
+  end
 end
