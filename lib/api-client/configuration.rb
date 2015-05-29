fa9cf43b1a7b7056f58082400879e396ac1213f0
Łukasz Śliwa <lukasz.sliwa@apptamers.com>
2015-05-29 20:47:31 +0200

    Added Token.

--- lib/api-client/configuration.rb
+++ lib/api-client/configuration.rb
@@ -1,13 +1,11 @@
-require "digest/sha1"
-
 class Api::Client::Configuration
-  attr_accessor :url, :version, :authorization_key, :secret_token
+  attr_accessor :url, :version, :token
 
   def url_with_version
     @url_with_version ||= [url, version].join '/'
   end
 
   def api_key
-    @api_key ||= Digest::SHA1.hexdigest("#{authorization_key}#{secret_token}")
+    @api_key ||= self.token
   end
 end
