1882b589c605c0eddc48cd995653a46e82f5f2b8
Łukasz Śliwa <lukasz.sliwa@apptamers.com>
2015-05-29 19:29:55 +0200

    Fixed headers hash.

--- lib/api-client/base.rb
+++ lib/api-client/base.rb
@@ -1,5 +1,5 @@
 class Api::Client::Base < ActiveResource::Base
   def self.headers
-    @headers ||= {'api_key': Api::Client.configuration.api_key}
+    @headers ||= {'api_key' => Api::Client.configuration.api_key}
   end
 end
