54b147423aa816dec182bb78b844009ab3068e1c
Łukasz Śliwa <lukasz.sliwa@apptamers.com>
2015-05-29 20:22:20 +0200

    API-KEY headers key

--- lib/api-client/base.rb
+++ lib/api-client/base.rb
@@ -1,5 +1,3 @@
 class Api::Client::Base < ActiveResource::Base
-  def self.headers
-    @headers ||= {'api_key' => Api::Client.configuration.api_key}
-  end
+  self.headers['API-KEY'] = Api::Client.configuration.api_key
 end
