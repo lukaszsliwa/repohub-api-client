9d1bd3b0fcfcfd4c5923adae82525386a2a0be2e
Łukasz Śliwa <lukasz.sliwa@apptamers.com>
2015-05-30 02:47:16 +0200

    Key and Developer fixes.

--- lib/api-client/developer.rb
+++ lib/api-client/developer.rb
@@ -3,12 +3,12 @@ class Api::Client::Developer < Api::Client::Base
 
   self.site = Api::Client.configuration.url_with_version
 
-  def self.allow(developer_id, repository_id)
-    Api::Client::Developer::Repository.put(repository_id, developer_id: developer_id)
+  def self.allow(developer_id, space_id, repository_id)
+    Api::Client::Developer::Repository.put(repository_id, developer_id: developer_id, space_id: space_id)
   end
 
-  def self.deny(developer_id, repository_id)
-    Api::Client::Developer::Repository.delete(repository_id, developer_id: developer_id)
+  def self.deny(developer_id, space_id, repository_id)
+    Api::Client::Developer::Repository.delete(repository_id, developer_id: developer_id, space_id: space_id)
   end
 
   autoload :Repository, 'api-client/developer/repository'
